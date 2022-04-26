070d1c12714439fec05a5eecc66a891bb7582b24
linn.mann@gmail.com <linn.mann@gmail.com>
2022-04-26 14:47:15 +0200

    added smaller functionality for disabled button

--- components/button.jsx
+++ components/button.jsx
@@ -1,9 +1,11 @@
 import styles from '../styles/components/button.module.css'
+import React, { useState } from 'react';
 
 export default function Button({children}){
+    const [isActive, setIsActive] = useState(null);
     return (
-        <div>
-            <button className={styles.btn}>{children}</button>
+        <div className='btn-container'>
+            <button className={styles.btn} disabled={!isActive}>{children}</button>
         </div>
         
     )

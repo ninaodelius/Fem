25d554a4ad37cab9798a3f39ced044aad2044722
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2022-04-27 11:43:38 +0200

    tog bort e

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ const handleClick = () => {
 
   return (
   <>
-    <Link href="/sign-in.jsx" passHref><Button />e</Link>
+    <Link href="/sign-in.jsx" passHref><Button /></Link>
     <Button disabled={false}>Nina hej</Button>
     <Link href="/sign-in.jsx">Sign in</Link>
     <br></br>

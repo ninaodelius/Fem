e6dda1249d8c967c57b0ec691c647e214186fb84
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2022-04-28 15:42:14 +0200

    hide show password

--- pages/auth/sign-in.jsx
+++ pages/auth/sign-in.jsx
@@ -1,8 +1,16 @@
 import Button from '../../components/button'
 import styles from '/styles/auth/signin.module.css'
 import Link from 'next/link'
+import usePasswordToggle from '../../hooks/usePassWordToggle.js'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' 
+import React, { useState, useEffect } from "react"
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
+import validate from "../../components/validateform"
 
-export default function SignIn(){
+const SignIn = () => {
+    const [showPassword, setShowPassword] = useState(true)
+    const [disabledButton, setDisabledButton] = useState(true)
     return(
         <div className={styles.loginform}>
             <div className={styles.title1}>WEME</div>
@@ -14,9 +22,10 @@ export default function SignIn(){
                     </label> 
                 </div>
                 <div className={styles.inputcontainer}>
-                <label>
-                    <input className={styles.input} type="text" name="password" placeholder="Lösenord" />
-                </label>
+                    
+                    <input className={styles.input} placeholder="Lösenord" type={showPassword ? "text" : "password"}  />
+                    <span className="password-toggle-icon" onClick={() => setShowPassword(showPassword => !showPassword)}>{showPassword ? <VisibilityIcon/> : <VisibilityOffIcon/>}</span>
+                
                 </div>
                 <div className={styles.checkboxcontainer}>
                 <input type="checkbox" id="remember" name="remember" value="Kom ihåg mig!"></input>
@@ -24,12 +33,15 @@ export default function SignIn(){
                 <p>Glömt lösenord?</p>
                 </div>
                 <div className={styles.buttoncontainer}>
-                    <Button>Logga in</Button>
+                    <Button disabled={disabledButton}>Logga in</Button>
                 </div>
                 <Link href="/auth/sign-up">Bli medlem</Link>
                 </form>
         </div>
 </div>
 
-    )
-}
+    );
+    
+};
+
+export default SignIn;

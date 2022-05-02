4f35dc192165aad4590daab144e73f609f557f2a
linn.mann@gmail.com <linn.mann@gmail.com>
2022-05-02 10:45:04 +0200

    api try

---
+++ data.js
@@ -0,0 +1,17 @@
+export const posts = [
+    {
+      id: '1',
+      author: 'Linn Mann',
+      text: 'Har någon information om API struktur i Next js?',
+    },
+    {
+        id: '3',
+        author: 'Linn Mann',
+        text: 'Har någon information om API struktur i Next js?',
+    },
+    {
+        id: '2',
+        author: 'Linn Mann',
+        text: 'Har någon information om API struktur i Next js?',
+    }
+  ]

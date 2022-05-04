3c8ff2662b7278dff108b7e3185377cd893d4fa4
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2022-05-04 16:35:56 +0200

    sign up step 3 css

--- components/sign-up-step3.jsx
+++ components/sign-up-step3.jsx
@@ -9,20 +9,23 @@ export default function SignUpThree() {
         <div className={styles.box}>
         <div className={styles.mentee}>
         <div class ={styles.menteeLogo}>M</div>
-        Mentee
+        <h4>Mentee</h4>
         Är någon som söker handledning och feedback av en kunnig inom branschen.
         </div>
         <div className={styles.mentor}>
         <div class ={styles.mentorLogo}>M</div>
-        Mentor
+        <h4>Mentor</h4>
         Någon som besitter merkunskaper och erfarenheter. En mentor handleder, stöttar och vägleder en mentee.</div>
         </div>
-        
+
         <p>Osäker i nuläget? Ingen fara! Du måste inte välja någon roll, men om du blir sugen i framtiden så går det bra att ange det i din profil. Så för nu kan du bara kicka på “Hoppa över”</p>
           <div className='buttoncontainer'>
           <Button><Link href='./account-created' passHref>Nästa</Link></Button>
           </div>
-          <Link href='./account-created'><a className={styles.link}><p>Hoppa över</p></a></Link>
+          <Link href='./account-created'><a className={styles.link}>
+          <div className={styles.fatPurpleP}>
+          Hoppa över</div>
+          </a></Link>
           
       </div>
 

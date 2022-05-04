8f227691760cd10a68ad994e30c3c425e0733e03
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2022-05-04 10:37:18 +0200

    added background and img for social media

--- components/footer.jsx
+++ components/footer.jsx
@@ -5,9 +5,9 @@ export default function Footer(){
     return(
         <footer className={styles.footerSection}>
         <div className={styles.socialFooter}>
-            <div className={styles.facebookLogo}><a href={'https://www.facebook.com'}>FÖLJ OSS PÅ FACEBOOK</a></div>
-            <div className={styles.instagramLogo}><a href={'https://www.instagram.com'}>FÖLJ OSS PÅ INSTAGRAM</a></div>
-            <div className={styles.emailLogo}>EMAILA OSS VID FRÅGOR</div>
+            <div className={styles.facebookLogo}><img src={'/images/facebook.png'}/><a href={'https://www.facebook.com'}>FÖLJ OSS PÅ FACEBOOK</a></div>
+            <div className={styles.instagramLogo}><img src={'/images/instagram.png'}/><a href={'https://www.instagram.com'}>FÖLJ OSS PÅ INSTAGRAM</a></div>
+            <div className={styles.emailLogo}><img src={'/images/email.png'}/>EMAILA OSS VID FRÅGOR</div>
             </div>
         </footer>
     )

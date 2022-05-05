7c6ec30edebb43afbfd9e72a3661211c1f300877
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2022-05-05 11:24:04 +0200

    scrollup function btnarrowup

--- components/btnArrowUp.jsx
+++ components/btnArrowUp.jsx
@@ -2,8 +2,13 @@ import styles from '../styles/components/btnarrowup.module.css'
 
 export default function btnArrowUp(){
 
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  } 
+
   return (
-    <button className={styles.btnArrowUp}></button>
+    <button className={styles.btnArrowUp} onClick={topFunction}></button>
   ) 
 
 }

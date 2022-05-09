694a4f04b12855f6f4b279c9ee20a408bd06c210
linn.mann@gmail.com <linn.mann@gmail.com>
2022-05-09 12:59:56 +0200

    changed from recieving children to prop.

--- components/post.jsx
+++ components/post.jsx
@@ -1,17 +1,17 @@
 import styles from '/styles/components/post.module.css'
 
-export default function Post({children}){
+export default function Post({post}){
     return(
         <div className={styles.postcontainer}>
             <div className={styles.infocontainer}>
                 <div className={styles.photowrap}>
                 <div className={styles.photo}><img src={'/images/Woman.svg'}/></div>
-                <div className={styles.namewrap }>Naomi Britton<div className={styles.m}>M</div></div>
+                <div className={styles.namewrap }>{post.author}<div className={styles.m}>M</div></div>
                 </div>
         </div>
         <div className={styles.textcontainer}> 
-                <div className={styles.textboxwrap}>{children.text}</div>
-                <p className={styles.menuwrap}>#FIGMA #PROTOTYPE #UX</p>
+                <div className={styles.textboxwrap}>{post.text}</div>
+                <p className={styles.menuwrap}>{post.tags}</p>
                 <hr className={styles.hr}/>
         </div>
         

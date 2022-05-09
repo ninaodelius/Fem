208d39a0a288068ee1cba3d42d9b7172bf43a247
Yaffet Kahsay <yaffet97@gmail.com>
2022-05-09 10:29:47 +0200

    update

--- components/button.jsx
+++ components/button.jsx
@@ -1,18 +1,9 @@
 import styles from '../styles/components/button.module.css'
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 export default function Button({children, disabled, type}){
     return (
-<<<<<<< HEAD
-                <>
-            <button className={styles.btn} disabled={disabled} >{children}</button>
-     
-            <button className={styles.btn} name="button" disabled={disabled} type="submit" >{children}</button>
-            </>
-=======
             <button className={styles.btn} name="button" disabled={disabled} type={type} >{children}</button>
->>>>>>> origin/main
-        
     )
     
 }

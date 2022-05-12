4b6480e34e337075adefa4b8d55f78765a7776d9
Yaffet Kahsay <yaffet97@gmail.com>
2022-05-12 10:37:42 +0200

    removed validations components

--- components/Validations/signUpOneValidation.js
+++
@@ -1,9 +0,0 @@
-import * as yup from 'yup';
-
-export const userForm = yup.object().shape({
-  name: yup.string().min(2).required(),
-  lastname: yup.string().min(2).required(),
-  email: yup.string().email().required(),
-  password: yup.string().min(4).max(10).required()
-
-})

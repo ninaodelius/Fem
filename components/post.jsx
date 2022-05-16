f14ac337ef13406ce757f937ac9aeb9e10e6682c
linn.mann@gmail.com <linn.mann@gmail.com>
2022-05-16 14:54:31 +0200

    changed post.jobtitle to post.title

--- components/post.jsx
+++ components/post.jsx
@@ -38,7 +38,7 @@ export default function Post({post}){
                     <div className={styles.photo}><img src={'/images/Woman.svg'}/></div>
                     <div className={styles.nameAndTitle}>
                         <div className={styles.namewrap }>{post.author}</div>
-                        <div className={styles.jobtitle}>{post.jobTitle}</div>  
+                        <div className={styles.jobtitle}>{post.title}</div>  
                     </div>
                 </div>
         </div>

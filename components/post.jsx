caad940f2c2588ffc1e59ea4ff2f5c51a518a5bb
linn.mann@gmail.com <linn.mann@gmail.com>
2022-05-17 09:24:35 +0200

    moved the sharepop box so it pops over content on feed page

--- components/post.jsx
+++ components/post.jsx
@@ -33,31 +33,31 @@ export default function Post({post}){
     const [sharePopup, setSharePopup] = useState(false);
 
     return(
-        <div className={styles.postcontainer}>
-            <div className={styles.infocontainer}>
-                <div className={styles.photowrap}>
-                    <div className={styles.photo}><img src={'/images/Woman.svg'}/></div>
-                    <div className={styles.nameAndTitle}>
-                        <div className={styles.namewrap }>{post.author}</div>
-                        <div className={styles.jobtitle}>{post.title}</div>  
+        <div>
+            <SharePop trigger={sharePopup} setTrigger={setSharePopup}></SharePop>
+            <div className={styles.postcontainer}>
+                <div className={styles.infocontainer}>
+                    <div className={styles.photowrap}>
+                        <div className={styles.photo}><img src={'/images/Woman.svg'}/></div>
+                        <div className={styles.nameAndTitle}>
+                            <div className={styles.namewrap }>{post.author}</div>
+                            <div className={styles.jobtitle}>{post.title}</div>  
+                        </div>
                     </div>
-                </div>
-        </div>
-        <div className={styles.textcontainer}> 
-                <div className={styles.textboxwrap}>{post.text}</div>
-                <Link href='./feed'><a className={styles.menuwrap}>{post.tags}</a></Link>
-                <hr className={styles.hr}/>
-                </div>
-                <div className={styles.interactContainer}>
-                <div className={styles.comment}><button className={styles.interactbutton}><img src={'/images/cmtbubble.png'}/>0</button></div>
-                    <div className={styles.like}><button className={styles.interactbutton} onClick={handleLike}>{showHeart ? <img src={'/images/filledHeart.png'}/> : <img src={'/images/emptyHeart.png'}/>}{likes}</button></div>
-                    <div className={styles.share}><button className={styles.interactbutton} onClick={() => setSharePopup(true)}><img src={'/images/shareicon.png'}/></button></div>
-                    <div className={styles.save}><button className={styles.interactbutton} onClick={handleShowSave}>{showSave ? <img src={'/images/savedicon.png'}/> : <img src={'/images/saveicon.png'}/>}</button></div>
+            </div>
+            <div className={styles.textcontainer}> 
+                    <div className={styles.textboxwrap}>{post.text}</div>
+                    <Link href='./feed'><a className={styles.menuwrap}>{post.tags}</a></Link>
+                    <hr className={styles.hr}/>
                     </div>
-                    <SharePop trigger={sharePopup} setTrigger={setSharePopup}></SharePop>
-        
+                    <div className={styles.interactContainer}>
+                    <div className={styles.comment}><button className={styles.interactbutton}><img src={'/images/cmtbubble.png'}/>0</button></div>
+                        <div className={styles.like}><button className={styles.interactbutton} onClick={handleLike}>{showHeart ? <img src={'/images/filledHeart.png'}/> : <img src={'/images/emptyHeart.png'}/>}{likes}</button></div>
+                        <div className={styles.share}><button className={styles.interactbutton} onClick={() => setSharePopup(true)}><img src={'/images/shareicon.png'}/></button></div>
+                        <div className={styles.save}><button className={styles.interactbutton} onClick={handleShowSave}>{showSave ? <img src={'/images/savedicon.png'}/> : <img src={'/images/saveicon.png'}/>}</button></div>
+                        </div>
 
+            </div>
         </div>
-
     )
 }

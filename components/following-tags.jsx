734e02e610a58f80256f642a7cd1b2cebf166112
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2022-05-20 10:59:59 +0200

    following tags

--- components/following-tags.jsx
+++ components/following-tags.jsx
@@ -1,4 +1,5 @@
 import styles from '/styles/components/followingtags.module.css'
+import Link from 'next/link'
 
 
 export default function FollowingTags(){
@@ -6,21 +7,37 @@ export default function FollowingTags(){
 
     return(
         <div className={styles.rightcontent}>
+
             <div className={styles.followcontainer}>
             <div className={styles.headercontainer}><div className={styles.lefttext}>Taggar du följer </div><div className={styles.righttext}>Redigera</div></div>
             <div className={styles.hr}></div>
+
             <div className={styles.followfeed}>
-            <div className={styles.leftcolumnpics}>
+
+            <div className={styles.rowcontent}>
             <div className={styles.tagpic}>
-                
+            <img src={'/images/portfolio.png'}/>
             </div>
+            <div className={styles.rowtext}>
+            <div className={styles.tagname}>
+            #PORTFOLIO
             </div>
-            <div className={styles.rightcolumntext}>
-                <div className={styles.tagname}>#PORTFOLIO</div>
-                <div className={styles.tagtext}>358 nya inlägg</div>
+            <div className={styles.tagtext}>358 nya inlägg</div>
+            </div>
             </div>
 
+            <div className={styles.rowcontent}>
+            <div className={styles.tagpic}>
+            <img src={'/images/cv.png'}/>
             </div>
+            <div className={styles.rowtext}>
+            <div className={styles.tagname}>#CV</div>
+            <div className={styles.tagtext}>278 nya inlägg</div>
+            </div>
+            </div>
+            
+            </div>
+
             <div className={styles.hr}></div>
             <div className={styles.bottomcontainer}>Visa fler taggar</div>
             </div>

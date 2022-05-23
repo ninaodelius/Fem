8371061012b8f2a44742dcc60e38ca10620a80ff
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2022-05-23 19:35:28 +0200

    footer text

--- components/footer.jsx
+++ components/footer.jsx
@@ -4,36 +4,38 @@ import styles from '../styles/components/footer.module.css'
 export default function Footer(){
     return(
         <div className={styles.footerSection}>
+
         <div className={styles.wemeLogo}>
         <img src={'/images/wemelogo.png'}/></div>
 
         <div className={styles.socialFooter}>
+
             <a href={'https://www.facebook.com'}>
             <div className={styles.facebookLogo}>
             <img src={'/images/facebook.png'}/>
+            </div>
             <div className={styles.socialMediaText}>
-          <span> FÖLJ OSS PÅ FACEBOOK </span>
+           FÖLJ OSS<br></br>PÅ FACEBOOK
             </div>
-            </div>
-</a>
+            </a>
 
             <a href={'https://www.instagram.com'}>
             <div className={styles.instagramLogo}>
             <img src={'/images/instagram.png'}/>
             </div>
             <div className={styles.socialMediaText}>
-          <span> FÖLJ OSS PÅ INSTAGRAM </span>
+          FÖLJ OSS<br></br>PÅ INSTAGRAM
             </div>
             </a>
 
             <div className={styles.emailLogo}>
             <img src={'/images/email.png'}></img>
             </div>
-
             <div className={styles.socialMediaText}>
-          <span>EMAILA OSS VID FRÅGOR </span>
+          EMAILA OSS<br></br>VID FRÅGOR 
             </div>
             </div>
+
         </div>
     )
 }

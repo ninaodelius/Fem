1648b4f8b9466ab53bb189abaa6778bcbec66688
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2022-05-23 17:02:52 +0200

    added focus button tags and link router

--- components/list.jsx
+++ components/list.jsx
@@ -2,22 +2,25 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import styles from '/styles/components/list.module.css'
 
-export default function List() {  
+export default function List() { 
+
+  const router = useRouter();
+
   return (
     <ul>
       <li className={styles.li}>
-        <Link href="/">
-          <a><img src={'/images/Househouse.png'}/>HEM</a>
+        <Link href="/feed">
+          <a className={router.pathname == "/feed" ? styles.active : ""}><img src={'/images/Househouse.png'}/>HEM</a>
         </Link>
       </li>
       <li className={styles.li}>
         <Link href="/explore">
-          <a><img src={'/images/Hashtaghashtag.png'}/>UTFORSKA</a>
+          <a className={router.pathname == "/explore" ? styles.active : ""}><img src={'/images/Hashtaghashtag.png'}/>UTFORSKA</a>
         </Link>
       </li>
       <li className={styles.li}>
         <Link href="/saved">
-          <a><img src={'/images/Savesave.png'}/>SPARAT</a>
+          <a className={router.pathname == "/saved" ? styles.active : ""}><img src={'/images/Savesave.png'}/>SPARAT</a>
         </Link>
       </li>
     </ul>

b67e40c43c305747044afb5385f173cf58f9ffd5
linn.mann@gmail.com <linn.mann@gmail.com>
2022-05-24 20:00:11 +0200

    wrote an api to fetch posts

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,9 +1,16 @@
-import { posts } from '../../data'
+import { db } from '../../firebase/firebaseConfig'
+import { collection, query, where , orderBy, getDocs } from "firebase/firestore";
 
-
-
-export default function handler(req, res) {
- 
-  res.status(200).json( posts )
-  
+export default async (req, res) => {
+  try {
+    const q = query(collection(db, "posts"), orderBy('createdAt', 'asc'))
+    const querySnapshot = await getDocs(q)
+    const postsData = querySnapshot.docs.map(post => ({
+      id: post.id,
+      ...post.data()
+    }));
+    res.status(200).json({ postsData });
+  } catch (e) {
+    res.status(400).end();
+  }
 }
